416178b0c79b70395bb9b977838b61df87e2ad2b
Ingrid Hastings <ingridhastings@gmail.com>
2015-07-07 12:51:49 -0500

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,13 @@ require 'securerandom'
 require 'faker'
 
  ikh = User.create!  username: "ikh",
+               email: "sample@example.com"
                password: "12345678",
                password_confirmation: "12345678",
                token: SecureRandom.hex(8)
 
  km = User.create!  username: "km",
+               email: "example@sample.com"
                password: "12345678",
                password_confirmation: "12345678",
                token: SecureRandom.hex(8)
@@ -20,7 +22,7 @@ require 'faker'
    10.times do
      sshhh = Sshhh.new
      sshhh.user = users.sample
+     sshhh.email = Faker::Internet.email
      sshhh.secrets = Faker::Lorem.sentence(3)
      sshhh.save!
    end
-
